ef64425bfc037403384dde45b1f2fa0c968612ed
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-01-16 14:52:36 -0200

    swap and cache cleanner

---
+++ clean_memory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# particao da swap
+PARTICAO=/dev/sda5
+
+# desliga a swap e liga novamente
+echo "limpando a swap: " $PARTICAO
+swapoff $PARTICAO
+swapon $PARTICAO
+
+# limpa o cache da memoria
+echo "limpando o cache da ram..."
+echo 3 > /proc/sys/vm/drop_caches
