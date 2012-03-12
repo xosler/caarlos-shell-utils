2d9e33a30ae8998659173f1e8c6c4867063dc16d
Alexandre Schossler <xosler@gmail.com>
2012-03-12 17:51:59 -0300

    simplificando

--- get_process_running_in_port.sh
+++ get_process_running_in_port.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 		
-PID=$(fuser $1/tcp)
-if [ -e $PID ]; then
-    echo "Nenhum processo encontrado para a porta $1"
-else
-    echo "Matando processo $PID"
-    kill -9 $PID
-fi
+fuser -k $1/tcp
+
